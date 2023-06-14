d5536ffbed30e8400d05902bb2b3385c4389ec1c
Joey Rideout <joey@rideout.net>
2023-06-13 20:28:14 -0400

    Done Day 30

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Joey Rideout on 2023-06-13.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
